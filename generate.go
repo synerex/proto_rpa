fa8e30c7fbd9088ff92819e769160b5e54e45f76
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2019-11-09 15:03:29 +0900

    update for new package name

--- generate.go
+++ generate.go
@@ -1,4 +1,4 @@
-package proto_rpa
+package proto_rpa // import "github.com/synerex/proto_rpa"
 
 // dummy go file for generating pb.go
 //go:generate protoc -I . --go_out=paths=source_relative:. meeting.proto
